f02f9cd0415b371e30b16646ea6f2bfc8c3cbd54
osamanazakat <usamanazakat97@gmail.com>
2023-02-04 10:46:17 +0500

    setting up the app component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import "./app.scss";
+import Home from "./pages/home";
+import Company from "./pages/company";
+import Contact from "./pages/contact";
+import Locataions from "./pages/locations";
+import Web from "./pages/web-design";
+import AppDesign from "./pages/app-design";
+import Graphic from "./pages/graphic-design";
+import Navbar from "./components/navbar/navbar";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route>
+        <Route />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/pages/app-design.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AppDesign = () => {
+  return <div>AppDesign</div>;
+};
+
+export default AppDesign;
