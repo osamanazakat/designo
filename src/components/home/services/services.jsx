d5d1c7e9c71ba53ef8dcb87c4477bfe4d9e73919
JohannesOostenbrink <oostenhan@gmail.com>
2023-02-05 19:47:13 +0700

    some styling on services section

--- src/components/home/services/services.jsx
+++ src/components/home/services/services.jsx
@@ -2,26 +2,26 @@ import "./services.scss";
 
 const Services = () => {
   return (
-    <div className="container services">
-      <div className="services_container">
+    <section className="services">
+      <div className="container services__container">
         <div className="services__container-left">
           <div className="services__left-web">
-            <h1>Web Design</h1>
-            <p>View Projects</p>
+            <h1 className="services__heading">Web Design</h1>
+            <p className="services__p">View Projects</p>
           </div>
         </div>
         <div className="services__container-right">
           <div className="services__top-app">
-            <h1>App Design</h1>
-            <p>View Projects</p>
+            <h1 className="services__heading">App Design</h1>
+            <p className="services__p">View Projects</p>
           </div>
           <div className="services__bottom-graphic">
-            <h1>Graphic Design</h1>
-            <p>View Projects</p>
+            <h1 className="services__heading">Graphic Design</h1>
+            <p className="services__p">View Projects</p>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
