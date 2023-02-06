5b6d1c8f780491d29128a62c6a52dbe52d627629
JohannesOostenbrink <oostenhan@gmail.com>
2023-02-06 20:18:52 +0700

    features commit

--- src/components/home/features/features.jsx
+++ src/components/home/features/features.jsx
@@ -1,4 +1,7 @@
 import "./features.scss";
+import Friendly from "../../../assets/home/desktop/illustration-friendly.svg";
+import Passionate from "../../../assets/home/desktop/illustration-passionate.svg";
+import Resourceful from "../../../assets/home/desktop/illustration-resourceful.svg";
 
 const Features = () => {
   const features = [
@@ -6,16 +9,19 @@ const Features = () => {
       id: 1,
       title: "PASSIONATE",
       desc: "Each project starts with an in-depth brand research to ensure we only create products that serve a purpose. We merge art, design, and technology into exciting new solutions.",
+      img: Passionate,
     },
     {
       id: 2,
       title: "RESOURCEFUL",
       desc: "Everything that we do has a strategic purpose. We use an agile approach in all of our projects and value customer collaboration. It guarantees superior results that fulfill our clients’ needs.",
+      img: Resourceful,
     },
     {
       id: 3,
       title: "FRIENDLY",
       desc: "Everything that we do has a strategic purpose. We use an agile approach in all of our projects and value customer collaboration. It guarantees superior results that fulfill our clients’ needs.",
+      img: Friendly,
     },
   ];
 
@@ -25,6 +31,7 @@ const Features = () => {
         {features.map(({ id, title, desc, img }) => {
           return (
             <div key={id} className="features__feature">
+              <img className="feature__img" src={img} />
               <h2 className="features__title">{title}</h2>
               <p className="feature__p">{desc}</p>
             </div>
