3fd84902f5f2e3e2ddf16324fb4347b29aa95bdd
JohannesOostenbrink <oostenhan@gmail.com>
2023-02-07 18:39:26 +0700

    Call to action component

---
+++ src/components/cta/cta.jsx
@@ -0,0 +1,24 @@
+import "./cta.scss";
+import Button from "../button/button";
+import { Link } from "react-router-dom";
+
+const Cta = () => {
+  return (
+    <section className="cta ">
+      <div className="cta__container container">
+        <div className="cta__text-container">
+          <h1 className="cta__heading">Let’s talk about your project</h1>
+          <p className="cta__p">
+            Ready to take it to the next level? Contact us today and find out
+            how our expertise can help your business grow.
+          </p>
+        </div>
+        <Link className="cta__button-container" to="/contact">
+          <Button text="get in touch" className="white" />
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default Cta;
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/home/header/header";
 import Services from "../components/home/services/services";
 import Features from "../components/home/features/features";
+import Cta from "../components/cta/cta";
 
 const home = () => {
   return (
@@ -9,6 +10,7 @@ const home = () => {
       <Header />
       <Services />
       <Features />
+      <Cta />
     </>
   );
 };
