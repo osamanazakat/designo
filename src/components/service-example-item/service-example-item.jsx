09282b0ff3a4310414eaa3fae7fd4fce287a6981
JohannesOostenbrink <oostenhan@gmail.com>
2023-02-09 20:16:29 +0700

    Working on styling

--- src/components/service-example-item/service-example-item.jsx
+++ src/components/service-example-item/service-example-item.jsx
@@ -4,15 +4,13 @@ const ServicesExampleItem = ({ serviceExample }) => {
   const { image, title, par } = serviceExample;
 
   return (
-    <section className="services-example-item">
-      <div className="services-example-item__container">
-        <div className="services-example-item__image">{image}</div>
-        <div className="services-example-item__body-container">
-          <h2>{title}</h2>
-          <p>{par}</p>
-        </div>
+    <div className="services-example-item">
+      <div className="services-example-item__image">{image}</div>
+      <div className="services-example-item__body-container">
+        <h2>{title}</h2>
+        <p>{par}</p>
       </div>
-    </section>
+    </div>
   );
 };
 
