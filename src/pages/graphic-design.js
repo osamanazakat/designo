ae0ecc6f34d65d4ab6d8a16347502635f1bdc14a
JohannesOostenbrink <oostenhan@gmail.com>
2023-02-11 09:24:27 +0700

    Complete services example sections

--- src/pages/graphic-design.js
+++ src/pages/graphic-design.js
@@ -1,12 +1,37 @@
 import ServicesHero from "../components/services-hero/services-hero";
+import ServicesExamples from "../components/services-examples/services-example";
+import BoxedWater from "../assets/graphic-design/desktop/image-boxed-water.jpg";
+import Change from "../assets/graphic-design/desktop/image-change.jpg";
+import Science from "../assets/graphic-design/desktop/image-science.jpg";
 
 const GraphicDesign = () => {
+  const ServicesExamplesData = [
+    {
+      id: 12,
+      title: "BOXED WATER",
+      image: <img style={{ width: "100%" }} src={BoxedWater} />,
+      par: "Solving the problem of poor air quality by filtering the air",
+    },
+    {
+      id: 13,
+      title: "Change",
+      image: <img style={{ width: "100%" }} src={Change} />,
+      par: "Product that lets you edit your favorite photos and videos at any time",
+    },
+    {
+      id: 14,
+      title: "SCIENCE",
+      image: <img style={{ width: "100%" }} src={Science} />,
+      par: "Get to meet your favorite internet superstar with the faceit app",
+    },
+  ];
   return (
     <>
       <ServicesHero
         heading="Graphic Design"
         par="We deliver eye-catching branding materials that are tailored to meet your business objectives"
       />
+      <ServicesExamples ServicesExamplesData={ServicesExamplesData} />
     </>
   );
 };
