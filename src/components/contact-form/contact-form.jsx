f65775fc6d7f4367f9efacc001ff153335b5c0a1
JohannesOostenbrink <oostenhan@gmail.com>
2023-02-18 21:16:42 +0700

    contact form submit button

--- src/components/contact-form/contact-form.jsx
+++ src/components/contact-form/contact-form.jsx
@@ -1,7 +1,37 @@
 import "./contact-form.scss";
+import { useState } from "react";
 import Button from "../button/button";
+import { init, send } from "emailjs-com";
 
 const ContactForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const messageBody = `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`;
+
+    send("service_2hhnfsc", "template_d0oijj8", {
+      from_name: name,
+      message_html: messageBody,
+      reply_to: email,
+    }).then(
+      (response) => {
+        console.log("Email sent:", response.status, response.text);
+        // Clear the form fields
+        setName("");
+        setEmail("");
+        setPhone("");
+        setMessage("");
+      },
+      (error) => {
+        console.error("Error sending email:", error);
+      }
+    );
+  };
+
   return (
     <section className="contact">
       <div className="contact__container container">
@@ -16,11 +46,31 @@ const ContactForm = () => {
         </div>
         <div className="contact__right">
           <form className="contact__form">
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email Address" />
-            <input type="text" placeholder="Phone" />
-            <input type="text" placeholder="Your Message" />
-            <Button className="white" text="Submit" />
+            <input
+              type="text"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="email"
+              placeholder="Email Address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Phone"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Your Message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <Button onClick={handleSubmit} className="white" text="Submit" />
           </form>
         </div>
       </div>
