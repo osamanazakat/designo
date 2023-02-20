cddc18e8d7cff55ddabf0eed62a0c081c6b82d16
osamanazakat <usamanazakat97@gmail.com>
2023-02-20 14:54:13 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/home/home";
+import Home from "./pages/home";
 import Company from "./pages/company";
 import Contact from "./pages/contact";
 import Locations from "./pages/locations";
